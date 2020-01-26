dd882787e4c2ff807052e35e3613db62d1aeae4c
ALEONDEV <53395716+Leonxv@users.noreply.github.com>
2020-01-26 16:19:29 +0100

    Update Main.php

--- src/Aleondev/payall/Main.php
+++ src/Aleondev/payall/Main.php
@@ -27,8 +27,8 @@ class Main extends PluginBase{
 								$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($player, $args[0]);
 								$spieler = $player->getName();
 								$player->sendMessage("§f[§4Pay§eAll§f] §r»§b Du hast durch den §4Pay§eAll§b von §r§6".$name." §e" .$args[0]. "$ §berhalten!");
-                                $player->addTitle("§bDas §4Pay§eall §bsystem \n §4hat \n§bAleondev\§4gecodet");
-                                $player->sendTip("§f[§4Pay§eAll§f] §r»§b Du hast durch den §4Pay§eAll§b von §r§6".$name." §e" .$args[0]. "$ §berhalten!");
+                                                                $player->addTitle("§bDas §4Pay§eall §bsystem \n §4hat \n§bAleondev\§4gecodet");
+                                                                $player->sendTip("§f[§4Pay§eAll§f] §r»§b Du hast durch den §4Pay§eAll§b von §r§6".$name." §e" .$args[0]. "$ §berhalten!");
 								}
 								$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->reduceMoney($sender, $setmoney);
 								$sender->sendMessage("§f[§4Pay§eAll§f] §r» §4Du hast §e".$setmoney."$ §4an die alle spieler gegeben!");
