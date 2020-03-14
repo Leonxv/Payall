c9bbc0130894b4df4816aba99ddb06b0c47bf60d
ALEONDEV <53395716+Leonxv@users.noreply.github.com>
2020-03-14 16:48:10 +0100

    Update Main.php

--- src/Aleondev/payall/Main.php
+++ src/Aleondev/payall/Main.php
@@ -9,41 +9,41 @@ use onebone\economyapi\EconomyAPI;
 
 
 class Main extends PluginBase{
+	
+	public const PREFIX = " §a[Payall] §6";
 
 	public function onEnable() : void {
 		$this->getLogger()->info("§4Pay§eAll §aon");
 	}
 
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-		if($command->getName() == "payall") {
-			if($sender->hasPermission("payall.command")) {
-				if(isset($args[0])) {
-					if(is_numeric($args[0])) {
-						$name = $sender->getName();
-						$setmoney = $args[0];
-						$money = EconomyAPI::getInstance()->myMoney($sender->getName());
-							if($money >= $setmoney) {
-								foreach ($this->getServer()->getOnlinePlayers() as $player) {
-								$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($player, $args[0]);
-								$spieler = $player->getName();
-								$player->sendMessage("§f[§4Pay§eAll§f] §r»§b Du hast durch den §4Pay§eAll§b von §r§6".$name." §e" .$args[0]. "$ §berhalten!");
-                                                                $player->addTip("§f[§4Pay§eAll§f] §r»§b Hat Aleondev Gecodet!");
-								}
-								$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->reduceMoney($sender, $setmoney);
-								$sender->sendMessage("§f[§4Pay§eAll§f] §r» §4Du hast §e".$setmoney."$ §4an die alle spieler gegeben!");
-							} else {
-								$sender->sendMessage("§f[§4Pay§eAll§f] §r» §cDu hast zu wenig Geld!");
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+	switch($command){
+	case "payall":
+    			if(isset($args[0])){
+    				if (is_numeric($args[0])) {
+    					$amount = $args[0];
+    					$anz = count($this->getServer()->getOnlinePlayers());
+    					$tanz = $anz - 1;
+    					$maxpay = $amount * $tanz;
+    					$mymoney = EconomyAPI::getInstance()->myMoney($sender);
+    					if($maxpay <= $mymoney) {
+							foreach ($this->getServer()->getOnlinePlayers() as $player) {
+								$name = $player->getName();
+								$iname = strtolower($name);
+								EconomyAPI::getInstance()->addMoney($iname, $amount);	
+								EconomyAPI::getInstance()->reduceMoney($sender, $amount);
+			
 							}
-					} else {
-						$sender->sendMessage("§f[§4Pay§eAll§f] §r» §cGib eine Zahl an!");
+							$this->getServer()->broadcastMessage(self::PREFIX . $sender->getName() . "§f hat §e" . $maxpay . " $ verteilt. Jeder hat: §e". $amount . "§f erhalten.");
+						}
+						else {$sender->sendMessage(self::PREFIX ."Du hast zu wenig Geld!");}
 					}
-				} else {
-					$sender->sendMessage("§f[§4Pay§eAll§f] §r» §6Benutze:§b /payall [Betrag]");
+					else {$sender->sendMessage(self::PREFIX ."Deine Eingabe ist keine Zahl!");}
+
 				}
-			} else {
-				$sender->sendMessage("§f[§4Pay§eAll§f] §r» §cDu hast keine Berechtigungen diesen Befehl zu nutzen!");
+				else {$sender->sendMessage(self::PREFIX ."Keine Summe angegeben!");}
 			}
-		}
-	return true;
-	}
-}
+            else {$sender->sendMessage(self::PREFIX ."Keine Berechtigung!");
+            } 
+	} 
+} 
